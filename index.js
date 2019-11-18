944d7738423925465b5bdd5336c69ef181144547
Automata2 <57898939+Automata2@users.noreply.github.com>
2019-11-18 20:33:36 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NjQ1OTE2MDYyMTUxNDc1MjEz.XdJjbw.dPd1b3p8ALuGcixG8_17jyRQJO0';
+
+bot.on('ready', () =>{
+    console.log('This bot is online!');
+})
+
+bot.on('message', message => {
+    if (message.content === 'Hi Momo Belia'){
+        message.reply('Hello');
+    }
+});
+
+bot.login(token);
